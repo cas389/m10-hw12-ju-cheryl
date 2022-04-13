8a35495b4dffde2c7c68731ee2f8b3cf1817f823
Cheryl Ju <cheryl.as.ju@gmail.com>
2022-04-13 19:38:33 -0400

    Created class named car and added a variable named mySweetRide

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # m10-hw12-ju-cheryl
 Assignment 12 for MMC6278
+
+
+## Issues While Completing Assignment
+
+## Outside Resources
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_class_init
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // YOUR CODE HERE
+
+// Create a class named Car in JavaScript.
+class Car {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+
+// Create a variable named mySweetRide and assign it a car created with your class
+var mySweetRide = new Car("Pontiac", "Fiero", 1988);
